69d171d6049eead1fa238c425eb3d7647715d3be
KoreaTech-System Programming 2016 <gkgkgy@koreatech.ac.kr>
2016-12-14 11:43:19 +0900

    Update LowCopy.c

--- Lab3/LowCopy.c
+++ Lab3/LowCopy.c
@@ -1,34 +1,42 @@
 #include <unistd.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <time.h>
 
 int main(int argc, char *argv[]) {
-	char block[1024];
-	int in, out;
-	int nread;
-	char input[30], output[30];
-
-	if(argc != 3) {
-		printf("Error! Please write right name.\n");
-		exit(1);
-	}
-
-	strcpy(input, argv[1]);
-	strcpy(output, argv[2]);
-
-	in = open(input, O_RDONLY);
-	out = open(output, O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
-
-	while((nread = read(in, block, sizeof(block))) > 0) {
-		write(out, block, nread);
-		printf(".");
-	}
-
-	close(in);
-	close(out);
-	printf("\n~finished~\n");
-	return 0;
+  int in, out;
+  char block[1024];
+  int nread;
+  char i_name[52], o_name[52];
+  
+  if(argc != 3) { //인자의 수가 조건에 맞지 않을 경우
+    fprintf(stderr, "error : put [input filename] [output filename]\n");
+    exit(1);
+  }
+  
+  if(strlen(argv[1]) > 50||strlen(argv[2]) > 50) {  //파일명 길이 제한
+    fprintf(stderr, "error : filename should not be over 100 byte\n");
+    exit(1);
+  }
+ 
+  strcpy(i_name, argv[1]);
+  strcpy(o_name, argv[2]);
+ 
+  in = open(i_name, O_RDONLY);
+  if(in == -1) { perror("Open"); exit(1);}
+ 
+  out = open(o_name, O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
+ 
+  while((nread = read(in, block, sizeof(block))) > 0 ) {
+    if(write(out, block, nread) != nread)
+      fprintf(stderr, "error : Write");
+    sleep(1);
+    printf(".");    
+  }
+  close(in);
+  close(out);
 }
+ //////////warning 수정, dot 출력 수정
